90da2ef205139aff5d7b8905f4ead0375906e168
nico <nico@nico.com>
2015-02-13 16:56:27 +0100

    exemples de multithreading

--- code-rpi/RS232/multithread_3.py
+++ code-rpi/RS232/multithread_3.py
@@ -5,8 +5,6 @@
 import threading
 import time
 
-exitFlag = 0
-
 class myThread (threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
@@ -15,16 +13,20 @@ class myThread (threading.Thread):
         self.counter = counter
     def run(self):
         print "Starting " + self.name
-        print_time(self.name, self.counter, 5)
-        print "Exiting " + self.name
+        # Get lock to synchronize threads
+        threadLock.acquire()
+        print_time(self.name, self.counter, 3)
+        # Free lock to release next thread
+        threadLock.release()
 
 def print_time(threadName, delay, counter):
     while counter:
-        if exitFlag:
-            thread.exit()
         time.sleep(delay)
         print "%s: %s" % (threadName, time.ctime(time.time()))
         counter -= 1
+
+threadLock = threading.Lock()
+threads = []
 
 # Create new threads
 thread1 = myThread(1, "Thread-1", 1)
@@ -34,4 +36,11 @@ thread2 = myThread(2, "Thread-2", 2)
 thread1.start()
 thread2.start()
 
+# Add threads to thread list
+threads.append(thread1)
+threads.append(thread2)
+
+# Wait for all threads to complete
+for t in threads:
+    t.join()
 print "Exiting Main Thread"
