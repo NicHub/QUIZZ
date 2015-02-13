64b88c0d962761015aec8db3dd55c96098c62e49
NicHub <nicolas.jeanmonod@gmail.com>
2015-02-13 15:55:59 +0100

    ajout de la synchronisation pour USB

--- gitsync.sh
+++ gitsync.sh
@@ -5,10 +5,29 @@
 
 echo $OSTYPE
 
+# RPi
 if [[ "$OSTYPE" == "linux-gnueabihf" ]]; then
     git pull ssh://nico@nicobook.local:22/Users/nico/Documents/projets/quizz/
+
+# Mac
 elif [[ "$OSTYPE" == "darwin14" ]]; then
-	git pull ssh://pi@$PIIP:22/home/pi/quizz
+
+	# Clé USB
+	if [[ "${PWD}" == "/Volumes/NICUSB/quizz" ]]; then
+		echo "USB"
+		git pull /Users/nico/Documents/projets/quizz/
+
+	# Macbook
+	elif [[ "${PWD}" == "/Users/nico/Documents/projets/quizz" ]]; then
+		echo "Mac"
+		git pull ssh://pi@$PIIP:22/home/pi/quizz
+
+	# Problème
+	else
+	    echo "Je suis perdu"
+	fi
+
+# Problème
 else
     echo "Y a un blême avec cet OS"
 fi
