3fecd7e3cd42e8477f9213a390a8421cad0ff2c4
NicHub <nicolas.jeanmonod@gmail.com>
2015-05-29 20:14:44 +0200

    test swift + photo platine

---
+++ code-arduino/wired_buttons_swift/TrackMix/TrackMix/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrackMix
+//
+//  Created by nico on 29.05.15.
+//  Copyright (c) 2015 nico. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
