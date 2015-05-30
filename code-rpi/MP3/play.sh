605df052c6645e5cff4b19787b28a9b02afe0a07
nico <nico@nico.com>
2015-05-30 15:48:01 +0200

    update play

--- code-rpi/MP3/play.sh
+++ code-rpi/MP3/play.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-amixer cset numid=1 -- -20
+# amixer cset numid=1 -- -20
 
-omxplayer ~/Musique/roxanne.mp3
+amixer cset numid=1 0
+
+# omxplayer ~/Musique/roxanne.mp3
+# omxplayer ~/Musique/Passenger\ -\ Holes\ \(Official\ Video\)\ \(mp4\ 1080p\).mp4 
+
+mpg321 /home/pi/Musique/roxanne.mp3 
