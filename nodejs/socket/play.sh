2245d48784ac0294a4432612cc43dfa0d1d93da0
nico <nico@nico.com>
2015-07-14 18:10:26 +0200

    cleanup

--- nodejs/socket/play.sh
+++ nodejs/socket/play.sh
@@ -2,6 +2,16 @@
 
 # amixer cset numid=1 -- -20
 
+# amixer cset numid=1 0
+
+# omxplayer ~/Musique/roxanne.mp3
+# omxplayer ~/Musique/Passenger\ -\ Holes\ \(Official\ Video\)\ \(mp4\ 1080p\).mp4
+
+# mpg321 /home/pi/Musique/roxanne.mp3
+
+
+# amixer cset numid=1 -- -20
+
 # omxplayer ~/quizz/code-rpi/MP3/Frog.aiff
 
 
